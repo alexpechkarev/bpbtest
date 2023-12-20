450826e33e587e42b55655e93edcb3b49195cf87
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-12-20 18:30:12 +0000

    added the added the error message div underneath the input

--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -34,17 +34,18 @@
                     <input id="email" name="email" type="email" autocomplete="email"  
                         class="block w-full border-0 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
-
+                <div>@error('email') {{ $message }} @enderror</div>
             </div>
-
+            
             <div>
                 <div class="mt-2">
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <input id="password" name="password" type="password" autocomplete="current-password" 
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                 </div>
+                <div>@error('password') {{ $message }} @enderror</div>
             </div>
-
+            
             <div>
                 <button type="submit"
                     class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-70">
