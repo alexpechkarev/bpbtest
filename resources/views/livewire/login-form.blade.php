b6b80548afa52787113c1bd058b2918350678482
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-12-21 17:03:55 +0000

    on invalid input, set the ring including with focus state to red on email and password inputs

--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -32,7 +32,7 @@
                 <div class="mt-2">
 
                     <input id="email" name="email" type="email" autocomplete="email" wire:model="email"  
-                        class="block w-full border-0 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        class="block w-full border-0 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('email') ring-red-500 focus:ring-red-500 @enderror">
                 </div>
                 <div>
                   @error('email')
@@ -45,7 +45,7 @@
                 <div class="mt-2">
                     <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <input id="password" name="password" type="password" autocomplete="current-password" wire:model="password"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 @error('password') ring-red-500 focus:ring-red-500 @enderror">
                 </div>
                 <div>
                   @error('password') 
